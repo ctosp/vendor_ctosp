882c9e3e5d6f7c1b57fe1473624163b9d2726553
Vamsee Krishnaa <46225107+A2O-G@users.noreply.github.com>
2019-04-04 13:03:58 +0530

    added ota

---
+++ config/ota.mk
@@ -0,0 +1,21 @@
+ifneq ($(IS_GENERIC_SYSTEM_IMAGE), true)
+ifeq ($(CUSTOM_BUILD_TYPE), OFFICIAL)
+
+ifeq ($(IS_GO_VERSION), true)
+CUSTOM_OTA_VERSION_CODE := pie_go
+else
+CUSTOM_OTA_VERSION_CODE := pie
+endif
+
+CUSTOM_PROPERTIES += \
+    org.ctosp.ota.version_code=$(CUSTOM_OTA_VERSION_CODE) \
+    sys.ota.disable_uncrypt=1
+
+PRODUCT_PACKAGES += \
+    Updates
+
+PRODUCT_COPY_FILES += \
+    vendor/aosp/config/permissions/org.ctosp.ota.xml:system/etc/permissions/org.ctosp.ota.xml
+
+endif
+endif
